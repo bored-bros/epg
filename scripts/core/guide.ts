8f61934ba9204c786d0106b5ac84155e309ea2f4
omar hayak <omar.hayak@gmail.com>
2024-12-06 15:14:00 +0100

    feat: add support json

--- scripts/core/guide.ts
+++ scripts/core/guide.ts
@@ -2,6 +2,7 @@ import { Collection, Logger, DateTime, Storage, Zip } from '@freearhey/core'
 import { Channel } from 'epg-grabber'
 import { XMLTV } from '../core'
 import { S3Storage } from './storage'
+import 'xml2js'
 
 type GuideProps = {
   channels: Collection
@@ -9,6 +10,12 @@ type GuideProps = {
   date: DateTime
   logger: Logger
   filepath: string
+}
+
+function nameToUpperCase(value, name){
+  console.log(value)
+  console.log(name)
+  return name
 }
 
 export class Guide {
@@ -35,6 +42,7 @@ export class Guide {
 
     let guideContent = ''
     let isXml = false
+    let isJson = false
     let isGzip = false
 
     // Get file extension and handle different cases
@@ -43,22 +51,37 @@ export class Guide {
       case filepath.endsWith('.xml'):
         isXml = true
         break
+      case filepath.endsWith('.json'):
+        isJson = true
+        break
       case filepath.endsWith('.xml.gz'):
       case filepath.endsWith('.xml.gzip'): 
         isXml = true
         isGzip = true
         break
+      case filepath.endsWith('.json.gz'):
+      case filepath.endsWith('.json.gzip'): 
+        isJson = true
+        isGzip = true
+        break
       default:
-        throw new Error('Invalid file extension. Only .xml, .xml.gz, .xml.gzip, and .json are supported')
+        throw new Error('Invalid file extension. Only {xml,json} or {xml,json}.{gz,gzip} are supported')
     }
 
+    const xmltv = new XMLTV({
+      channels: channels,
+      programs: this.programs,
+      date: this.date
+    })
+
     if (isXml) {
-      const xmltv = new XMLTV({
-        channels: channels,
-        programs: this.programs,
-        date: this.date
-      })
       guideContent = xmltv.toString()
+    }
+
+    if (isJson) {
+      const xml2js = require('xml2js')
+      const result = await xml2js.parseStringPromise(xmltv.toString(), {mergeAttrs: true, explicitArray: false, trim: true, normalize: true})
+      guideContent = JSON.stringify(result.tv, null, 2)
     }
 
     if (isGzip) {
